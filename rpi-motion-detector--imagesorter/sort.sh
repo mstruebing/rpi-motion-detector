6144c559b2739b293fba1162aca2fb5700461812
Alexander Viertel <alexander@mail.aviertel.de>
2017-09-08 10:35:25 +0200

    added thumbnail generation

--- rpi-motion-detector--imagesorter/sort.sh
+++ rpi-motion-detector--imagesorter/sort.sh
@@ -13,6 +13,19 @@ if [ $COUNT != 0 ]; then
 
 		DIR=../$HOST/$YEAR/$MONTH/$DAY
 
+		IMAGE_SIZE=`file -b $f | sed 's/ //g' | sed 's/,/ /g' | awk  '{print $2}'`
+		WIDTH=`identify -format "%W" "$f"`
+		HEIGHT=`identify -format "%H" "$f"`
+		echo size: $WIDTH $HEIGHT
+		# If the image width is greater that 200 or the height is greater that 150 a thumb is created
+		if [ $WIDTH -ge  201 ] || [ $HEIGHT -ge 201 ]; then
+	        	#This line convert the image in a 200 x 150 thumb
+	        	filename=$(basename "$f")
+		        extension="${filename##*.}"
+        		filename="${filename%.*}"
+		        convert -sample 200x200 "$f" "$DIR/${filename}_thumb.${extension}"
+     		fi
+
 		if [ ! -d "$DIR" ]; then
 			mkdir -p "$DIR"
 		fi
